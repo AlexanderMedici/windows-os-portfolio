672888dca7102623c40046fe30491ce4a51f9151
Alexander M <hellomedici@outlook.com>
2026-01-09 11:13:32 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: 'https://www.alexmedici.online/',
+  base: process.env.RENDER
+    ? 'https://windows-os-portfolio.onrender.com/'
+    : 'https://www.alexmedici.online/',
   plugins: [react(),
   tailwindcss(),
   ]
